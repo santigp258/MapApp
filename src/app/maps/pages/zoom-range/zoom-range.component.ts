0c7588fb9a511ea5b3cc24429c945ef6066ee6ca
Santiago <santigp258@gmail.com>
2021-02-09 15:07:50 -0500

    styles in zoom-range

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+    .map-container{
+      width: 100%;
+      height: 100%
+    }
+
+    .row{
+      width: 250px;
+      background-color: white;
+      border-radius: 5px;
+      bottom: 50px;
+      margin: 5px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+
+    @media (max-width: 480px){
+      .row{
+        width: 150px;
+      }
+    }   
+     `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +35,12 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var map = new mapboxgl.Map({
+      container: 'mapRange',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.97396426489313, 4.5759589923197455], 
+      zoom: 14
+    });
   }
 
 }

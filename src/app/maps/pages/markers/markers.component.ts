eb8e8075514f8adda3adc97dc4f261aadc695c88
Santiago <santigp258@gmail.com>
2021-02-09 17:04:02 -0500

    markers component

--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -1,16 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
   styles: [
-  ]
+    `
+      .map-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .list-group {
+        position: fixed;
+        top: 20px;
+        right: 20px;
+        z-index: 99;
+      }
+
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class MarkersComponent implements OnInit {
+export class MarkersComponent implements AfterViewInit {
+  @ViewChild('map') divMap!: ElementRef;
+  map!: mapboxgl.Map;
+  zoomLevel: number = 15;
+  center: [number, number] = [-75.97396426489313, 4.5759589923197455];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    /*  const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = 'Hello World';
+
+    new mapboxgl.Marker({
+      element: markerHtml
+    })
+    .setLngLat(this.center)
+    .addTo(this.map) */
   }
 
+  addMarker() {
+    const color = '#xxxxxx'.replace(/x/g, (y) =>
+      ((Math.random() * 16) | 0).toString(16)
+    );
+
+    const newMarker = new mapboxgl.Marker({
+      draggable: true,
+      color: color
+    })
+      .setLngLat(this.center)
+      .addTo(this.map);
+  }
+
+  goMarker() {}
 }

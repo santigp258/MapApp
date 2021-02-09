9b10610b8227e62226fcb1aa1ad2e8f7847e8fa5
Santiago <santigp258@gmail.com>
2021-02-09 16:20:02 -0500

    clean events, OnDestroy

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,71 +12,84 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './zoom-range.component.html',
   styles: [
     `
-    .map-container{
-      width: 100%;
-      height: 100%
-    }
-
-    .row{
-      width: 250px;
-      background-color: white;
-      border-radius: 5px;
-      bottom: 50px;
-      margin: 5px;
-      padding: 10px;
-      position: fixed;
-      z-index: 999;
-    }
-
-    @media (max-width: 480px){
-      .row{
-        width: 150px;
+      .map-container {
+        width: 100%;
+        height: 100%;
       }
-    }   
-     `
-  ]
-})
-export class ZoomRangeComponent implements AfterViewInit {
 
+      .row {
+        width: 250px;
+        background-color: white;
+        border-radius: 5px;
+        bottom: 50px;
+        margin: 5px;
+        padding: 10px;
+        position: fixed;
+        z-index: 999;
+      }
+
+      @media (max-width: 480px) {
+        .row {
+          width: 150px;
+        }
+      }
+    `,
+  ],
+})
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
   @ViewChild('map') divMap!: ElementRef;
   map!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-75.97396426489313, 4.5759589923197455];
+
   constructor() {
     console.log('const', this.divMap);
-   }
-
-   
-   //para que se cree después del component
-  ngAfterViewInit(): void {
-    this.map= new mapboxgl.Map({
-      container: this.divMap.nativeElement,
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-75.97396426489313, 4.5759589923197455], 
-      zoom: this.zoomLevel
-    });
-
-    this.map.on('zoom', (event)=>{
-     this.zoomLevel = this.map.getZoom();
-    })
-
-    this.map.on('zoomend', (event)=>{
-      if(this.map.getZoom() > 18){
-        this.map.zoomTo(18);
-      }
-     })
   }
 
-  zoomOut(){
+  ngOnDestroy(){
+    //clean event
+    this.map.off('zoom', ()=>{});
+    this.map.off('zoomend', ()=>{});
+    this.map.off('zoommove', ()=>{});
+  }
+
+  //para que se cree después del component
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.center,
+      zoom: this.zoomLevel,
+    });
+
+    //map zomm
+    this.map.on('zoom', (event) => {
+      this.zoomLevel = this.map.getZoom();
+    });
+
+    this.map.on('zoomend', (event) => {
+      if (this.map.getZoom() > 18) {
+        this.map.zoomTo(18);
+      }
+    });
+
+    //move map
+    this.map.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [lng, lat];
+    });
+  }
+
+  zoomOut() {
     this.map.zoomOut();
   }
 
-  zoomIn(){
+  zoomIn() {
     this.map.zoomIn();
   }
 
-  zoomChange(value: string){
+  zoomChange(value: string) {
     this.map.zoomTo(Number(value));
   }
-
-
 }

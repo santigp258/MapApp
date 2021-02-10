19e15ed73a94a5a469f35294ab3d52c018a61046
Santiago <santigp258@gmail.com>
2021-02-09 19:57:12 -0500

    finish propierties

--- src/app/maps/components/min-map/min-map.component.ts
+++ src/app/maps/components/min-map/min-map.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-min-map',
   templateUrl: './min-map.component.html',
   styles: [
-  ]
+    `
+      div {
+        width: 100%;
+        height: 150px;
+        margin: 0;
+      }
+    `,
+  ],
 })
-export class MinMapComponent implements OnInit {
+export class MinMapComponent implements AfterViewInit {
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('map') divMap!: ElementRef;
+  constructor() {}
+  ngAfterViewInit(): void {
+    var map = new mapboxgl.Map({
+      container: this.divMap.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+    });
 
-  constructor() { }
-
-  ngOnInit(): void {
+    new mapboxgl.Marker()
+    .setLngLat(this.lngLat)
+    .addTo(map)
   }
-
+  
 }
--- src/app/maps/pages/propierties/propierties.component.ts
+++ src/app/maps/pages/propierties/propierties.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Propierty {
+  title: string;
+  description: string;
+  lngLat: [number, number];
+}
 @Component({
   selector: 'app-propierties',
   templateUrl: './propierties.component.html',
@@ -8,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropiertiesComponent implements OnInit {
 
+  Propierties: Propierty[] = [
+    {
+      title: 'Casa residencial, Canadá',
+      description: 'Bella propiedad en Katana, Canadá',
+      lngLat: [ -75.92722289474008, 45.280015511264466]
+    },
+    {
+      title: 'Casa de playa, México',
+      description: 'Hermosa casa de playa en Acapulco, México',
+      lngLat: [ -99.91287720907991, 16.828940930185748]
+    },
+    {
+      title: 'Apartamento, Argentina',
+      description: 'Lujoso apartamento en el corazón de Buenos Aires, Argentina',
+      lngLat: [ -58.430166677283445, -34.57150108832866 ]
+    },
+    {
+      title: 'Local comercial, España',
+      description: 'Local comercial disponible en Madrid, España, cerca de El Jardín Secreto.',
+      lngLat: [ -3.7112735618380177, 40.42567285425766 ]
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
